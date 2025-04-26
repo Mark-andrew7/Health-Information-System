d7b62abb1bb97552d59cb83507e115b81dccb0e5
Mark-andrew7 <kamaumark19@gmail.com>
2025-04-26 04:38:28 +0000

    Added .env to gitignore

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDb = require('./config/db');
 const healthRoutes = require('./routes/healthRoutes');
+const clientRoutes = require('./routes/clientRoutes');
+const enrollmentRoutes = require('./routes/enrollmentRoutes');
 
 dotenv.config();
 const app = express();
@@ -13,6 +15,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', healthRoutes);
+app.use('/api', clientRoutes);
+app.use('/api', enrollmentRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- backend/src/controllers/healthProgram.js
+++ backend/src/controllers/healthProgram.js
@@ -1,11 +1,11 @@
-const HealthProgram = require('./models/HealthProgram');
+const HealthProgram = require('../models/HealthProgram');
 
 exports.createProgram = async(req, res) => {
     const { name, description } = req.body;
     if (!name) return res.status(400).json({error: "Name is required"});
     try {
         const program = new HealthProgram({ name, description });
-        await HealthProgram.save();
+        await program.save();
         res.status(201).json(program);
     } catch (err) {
         res.status(400).json({ error: err.message });
